786877fd54b2e6ea8f092348fa655abecc6c7c0b
MCattaneo93 <MCattaneo93@gmail.com>
2019-12-18 08:38:39 -0500

    Updated to include search box
    Updated php to have specific function to return sql results

--- cs331_search_result.php
+++ cs331_search_result.php
@@ -6,50 +6,56 @@ error_reporting(E_ALL);
 ini_set('display_errors', true);
 
 //obtain data from SQL database
-$search_term = $_GET['wordEntered'];
-//MariaDB server information
-$servername = "mars.cs.qc.cuny.edu";
-$username = "cama0204";
-$password = "23830204";
-$dbname = "cama0204";
-//create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-//if unsuccesful connection, kill connection
-if($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
+if (isset($_GET['submit'])) {
+    $search_term = $_GET['wordEntered'];
+    
+    returnSearchData($search_term);
 }
-//SQL Query to return data
-$sql = "
+
+function returnSearchData($search_term)
+{
+    //MariaDB server information
+    $servername = "mars.cs.qc.cuny.edu";
+    $username   = "cama0204";
+    $password   = "23830204";
+    $dbname     = "cama0204";
+    //create connection
+    $conn       = new mysqli($servername, $username, $password, $dbname);
+    //if unsuccesful connection, kill connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    //SQL Query to return data
+    $sql = "
 
 SELECT *
 FROM page, word, page_word
 WHERE page.pageId = page_word.pageId
 AND word.wordId = page_word.wordId
 AND word.wordName = ‘" . wordEntered . "‘
-ORDER BY freq desc;"
+ORDER BY freq desc;";
+    
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "
+            <div id=\"searchResults\" class=\"results\"> + <input type=\"checkbox\" name=\"check\">" . "<h4>" . $row[title] . "</h4>" . "<a href=" . $row["url"] . ">" . $row["url"] . "</a>" . "<p>" . $row["description"] . "</p>" . "<div>;
 
-$result = $conn->query($sql);
-
-if($result->num_rows > 0){
-	while($row = $result->fetch_assoc()){
-		echo
-		"
-			<div id=\"searchResults\" class=\"results\"> + <input type=\"checkbox\" name=\"check\">" .
-            "<h4>" .  $row[title] . "</h4>" .
-            "<a href=" . $row["url"] . ">". $row["url"] . "</a>" .
-            "<p>" . $row["description"] . "</p>" . "<div>;
-
-		";
-
-	}
-
-
-}else{
-	echo "No results, maybe try indexing some pages...";
+        ";
+            
+        }
+        
+        
+    } else {
+        echo "No results, maybe try indexing some pages...";
+    }
+    
+    
+    
+    $conn->close();
+    
+    
+    
 }
-
-
-
-$conn->close();
 ?>
